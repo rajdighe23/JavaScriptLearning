6e25ef13c4176ed0cbd996c87c9e545862ad8062
rajdighe23 <rajsdighe2002@gmail.com>
2025-11-21 16:09:13 +0530

    Create object to store score

--- Projects/Cricket Game/cricket.js
+++ Projects/Cricket Game/cricket.js
@@ -14,33 +14,57 @@ function generateComputerChoice() {
 function getResult(userMove, computerMove) {
   if (userMove === "Bat") {
     if (computerMove === "Ball") {
+      score.win++;
       return "User Won.";
     } else if (computerMove === "Bat") {
+      score.tie++;
       return `It's a tie.`;
     } else if (computerMove === "Stump") {
+      score.lost++;
       return "Computer has won.";
     }
   } else if (userMove === "Ball") {
     if (computerMove === "Ball") {
+      score.tie++;
       return `It's a tie.`;
     } else if (computerMove === "Bat") {
+      score.lost++;
       return `Computer has won.`;
     } else if (computerMove === "Stump") {
+      score.win++;
       return "User won.";
     }
   } else if (userMove === "Stump") {
     if (computerMove === "Ball") {
+      score.lost++;
       return `Computer has won.`;
     } else if (computerMove === "Bat") {
+      score.win++;
       return `User Won`;
     } else if (computerMove === "Stump") {
+      score.tie++;
       return `It's a tie.`;
     }
   }
 }
 
 function showResult(userMove, computerMove, result) {
+  // console.log(score);
   alert(
-    `You have chosen ${userMove}. Computer choice is ${computerMove} and ${result}`
+    `You have chosen ${userMove}. Computer choice is ${computerMove} 
+
+    ${result}
+
+    ${score.displayScore()}`
   );
 }
+
+// Create object to store score
+let score = {
+  win: 0,
+  lost: 0,
+  tie: 0,
+  displayScore: function () {
+    return `No of matches Won:${score.win}, Lost:${score.lost}, Tie:${score.tie}`;
+  },
+};
